21de28a02cd3cc156052ccf85001c3609fa468d1
rakibaakter <rakibaakter635@gmail.com>
2023-09-15 22:33:37 +0600

    header and data in json object created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="py-10">
+            <h1 className="text-3xl font-bold text-center">Course Regitration</h1>
+        </div>
+    );
+};
+
+export default Header;
